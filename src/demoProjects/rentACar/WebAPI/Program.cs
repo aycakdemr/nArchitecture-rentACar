80034bceaa4c3e565427aa9a539ac75b01b0477b
Ayça Akdemir <mabeaaa@gmail.com>
2022-08-16 09:58:48 +0300

    16.08

--- src/demoProjects/rentACar/WebAPI/Program.cs
+++ src/demoProjects/rentACar/WebAPI/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddPersistenceServices(builder.Configuration);
 //builder.Services.AddHttpContextAccessor();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer(); 
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
