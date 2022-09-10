8bb39371444a1251a932f5b49c5dcfa48f877b04
Ayça Akdemir <mabeaaa@gmail.com>
2022-09-10 17:26:37 +0300

    add dynamic search

---
+++ src/demoProjects/rentACar/Application/Features/Models/Queries/GetListModelByDynamic/GetListModelByDynamicQuery.cs
@@ -0,0 +1,45 @@
+﻿using Application.Features.Models.Models;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Requests;
+using Core.Persistence.Dynamic;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Models.Queries.GetListModelByDynamic
+{
+    public class GetlistModelByDynamicQuery : IRequest<ModelListModel>
+    {
+        public Dynamic Dynamic { get; set; }
+        public PageRequest PageRequest { get; set; }
+
+        public class GetlistModelByDynamicQueryHandler : IRequestHandler<GetlistModelByDynamicQuery, ModelListModel>
+        {
+            private readonly IMapper _mapper;
+            private readonly IModelRepository _modelRepository;
+
+            public GetlistModelByDynamicQueryHandler(IMapper mapper, IModelRepository modelRepository)
+            {
+                _mapper = mapper;
+                _modelRepository = modelRepository;
+            }
+
+            public async Task<ModelListModel> Handle(GetlistModelByDynamicQuery request, CancellationToken cancellationToken)
+            {
+                IPaginate<Model> models = await _modelRepository.GetListByDynamicAsync(request.Dynamic ,include:
+                                                 m => m.Include(c => c.Brand),
+                                                 index: request.PageRequest.Page,
+                                                 size: request.PageRequest.PageSize);
+                ModelListModel model = _mapper.Map<ModelListModel>(models);
+                return model;
+            }
+        }
+    }
+}
---
+++ src/demoProjects/rentACar/WebAPI/Controllers/ModelsController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Features.Brands.Models;
+using Application.Features.Models.Models;
+using Application.Features.Models.Queries.GetlistModel;
+using Application.Features.Models.Queries.GetListModelByDynamic;
+using Core.Application.Requests;
+using Core.Persistence.Dynamic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelsController :  BaseController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetlistModelQuery getListModelQuery = new() { PageRequest = pageRequest };
+            ModelListModel result = await Mediator.Send(getListModelQuery);
+            return Ok(result);
+        }
+        [HttpPost("GetList/ByDynamic")]
+        public async Task<IActionResult> GetListByDynamic([FromQuery] PageRequest pageRequest,[FromBody] Dynamic dynamic)
+        {
+            GetlistModelByDynamicQuery getListModelbyDynamicQuery = new() { PageRequest = pageRequest ,Dynamic = dynamic};
+            ModelListModel result = await Mediator.Send(getListModelbyDynamicQuery);
+            return Ok(result);
+        }
+    }
+}
